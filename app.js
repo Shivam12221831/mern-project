e8e9cf347120ec106be60674dc8d6859d23e4c1b
Shivam12221831 <shiva954882@gmail.com>
2025-02-03 23:26:57 +0530

    adding css in phase1-partA

--- app.js
+++ app.js
@@ -4,77 +4,80 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req,res) => {
-    res.send("Hi, I am root");
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
 // Index Route
-app.get("/listings", async (req,res) => {
-    let allListings = await Listing.find();
-    res.render("listings/index.ejs", {allListings});
-    // res.send("data fetched!");
-})
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find();
+  res.render("listings/index.ejs", { allListings });
+  // res.send("data fetched!");
+});
 
-// New Route 
-app.get("/listings/new", (req,res) => {
-    res.render("listings/new.ejs");
-})
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
 // Show Route
 app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    let listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-})
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
 // Create Route
-app.post("/listings", async(req,res) => {
-    const listing = new Listing(req.body.listing);
-    await listing.save();
-    res.redirect("/listings");
-})
+app.post("/listings", async (req, res) => {
+  const listing = new Listing(req.body.listing);
+  await listing.save();
+  res.redirect("/listings");
+});
 
 // Edit Route
-app.get("/listings/:id/edit", async (req,res) => {
-    let {id} = req.params;  
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 // Update Route
-app.put("/listings/:id", async(req,res) => {
-    const {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
 // Delete Route
-app.delete("/listings/:id", async(req,res) => {
-    const {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-})
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
 // app.get("/testListing", async(req,res) => {
 //     let sampleList = new Listing({
@@ -91,5 +94,5 @@ app.delete("/listings/:id", async(req,res) => {
 
 const port = 8080;
 app.listen(port, () => {
-    console.log(`server is running on port : ${port}`);
+  console.log(`server is running on port : ${port}`);
 });
--- init/index.js
+++ init/index.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
 main()
-    .then(() => {
-        console.log("connceted to db");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("connceted to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized successfully");
-}
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized successfully");
+};
 
 initDB();
